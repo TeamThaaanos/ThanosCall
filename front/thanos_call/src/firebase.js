9f3af6c685dab6610b46fb2e4714475b6a3169b8
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2025-02-12 16:01:24 +0900

    init : 파이어베이스 연동

---
+++ front/thanos_call/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCF3kR7EqOBTc7145gsjJMQyoFj3Knwyjk",
+    authDomain: "thanoscall-30729.firebaseapp.com",
+    projectId: "thanoscall-30729",
+    storageBucket: "thanoscall-30729.firebasestorage.app",
+    messagingSenderId: "422555274700",
+    appId: "1:422555274700:web:726ffdd4b3f1fdf0242169",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
