c69c54b7f0dccd7bcf3044f8faeb8fcedd802c4b
ilchan123 <dlfcks9@gmail.com>
2025-02-15 02:55:47 +0900

    feat: 상단 바 컴포넌트
    1. 달력
    2. 정렬(3개)
    3. 검색

---
+++ front/thanos_call/src/components/CSListManage/CSList_button_All.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import DateRangePicker from "./DateRangePicker"
+import DropdownButtonGroup from "./DropdownButtonGroup"
+import SearchBox from "../mingi/SearchBox"
+
+const CS_button_bar = () => {
+  return (
+    <div style={styles.container}>
+      <DateRangePicker />
+
+      <DropdownButtonGroup />
+
+      <SearchBox />
+    </div>
+  )
+}
+
+const styles = {
+  container: {
+    display: "flex",
+    alignItems: "center",
+    gap: "12px",
+    backgroundColor: "#1E3A8A",
+    padding: "10px 14px",
+    borderRadius: "8px",
+    height: "45px",
+  },
+}
+
+export default CS_button_bar
