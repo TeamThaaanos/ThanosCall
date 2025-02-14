67b1e0e125e45497512fdc60b7656bd4e33e7692
ilchan123 <dlfcks9@gmail.com>
2025-02-15 02:57:19 +0900

    feat: 문자열 string로 변환

---
+++ front/thanos_call/src/config/string.js
@@ -0,0 +1,26 @@
+export const STRINGS = {
+  BASIC_FRAME: {
+    CSButtonGroup: {
+      CS_MANAGE: "CS 내역 관리",
+      CS_MENUAL: "CS 메뉴얼 확인",
+      CS_DATA: "CS 데이터 분석",
+    },
+    CSPROFILECARD: {
+      USER_AGE: "나이 : ",
+      USER_AGE_ACTUAL: "만 {age}세",
+      USER_TODAY_COUNSEL_COUNT: "당일 상담 건수 : ",
+      USER_TODAY_COUNSEL_COUNT_ACTUAL: "{cases}통",
+      USER_TODAY_COUNSEL_TIME: "당일 상담 시간 :",
+    },
+  },
+  CSLISTMANAGE: {},
+  LOGIN_FRAME: {
+    LOGIN: {
+      LOGIN: "로그인",
+    },
+    LOGIN_FORM: {
+      EMPLOYEE_NUMBER: "사원 번호 or 사업자 번호",
+      PASSWORD: "비밀번호",
+    },
+  },
+}
