be440c0a18bed2be5e14512473fcaf49b4943330
ilchan123 <dlfcks9@gmail.com>
2025-02-17 09:14:03 +0900

    fix: 오른쪽 레이어에 컴포넌트 추가

--- front/thanos_call/src/components/basic_frame/CS_Right_layer.jsx
+++ front/thanos_call/src/components/basic_frame/CS_Right_layer.jsx
@@ -1,11 +1,23 @@
 import React from "react"
 import LogoLetter from "../../assets/images/Logo_letter_finger.svg"
+import SearchableDashboard from "../boxes/SearchableDashboard"
 
 const CSRightLayer = () => {
   return (
     <div style={styles.container}>
+      {/* 오른쪽 상단 로고 */}
       <div style={styles.topRight}>
         <img src={LogoLetter} alt="Logo" style={styles.logo} />
+      </div>
+
+      {/* 중앙 콘텐츠 박스 */}
+      <div style={styles.contentWrapper}>
+        <div style={styles.leftBox}>
+          <SearchableDashboard />
+        </div>
+        <div style={styles.rightBox}>
+          {/* 여기에 다른 박스 또는 내용 추가 가능 */}
+        </div>
       </div>
     </div>
   )
@@ -14,22 +26,38 @@ const CSRightLayer = () => {
 const styles = {
   container: {
     width: "100%",
-    height: "100vh",
+    height: "100vhs",
     backgroundColor: "#003E79",
-    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: "150px",
+    position: "relative", // 절대 위치 요소의 기준이 됨
   },
   topRight: {
-    position: "absolute",
-    top: "-100px",
-    right: "20px",
-    display: "flex",
-    alignItems: "center",
-    gap: "10px",
+    position: "absolute", // 절대 위치 설정
+    top: "-130px", // 상단 여백
+    right: "30px", // 오른쪽 끝에 배치
   },
   logo: {
-    width: "320px",
-    height: "320px",
+    width: "300px", // 로고 크기 조정
+    height: "300px",
     objectFit: "contain",
+  },
+  contentWrapper: {
+    display: "flex",
+    maxWidth: "1200px",
+    width: "100%",
+    justifyContent: "space-between",
+    gap: "20px",
+  },
+  leftBox: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+  },
+  rightBox: {
+    flex: 1,
   },
 }
 
